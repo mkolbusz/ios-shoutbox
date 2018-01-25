9a2f00c4a487ff5d66f7a83b99363d6b8ff6dfb7
Michal Kolbusz <mkolbusz@Michals-iMac.local>
2018-01-25 01:17:39 +0100

    Initial Commit

---
+++ lab02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab02
+//
+//  Created by Michal Kolbusz on 1/25/18.
+//  Copyright © 2018 Michal Kolbusz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
